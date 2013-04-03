6a058ec682b61436a4159bdbc9ecacc68b9ecddf
Tyler D <Jensha@gmail.com>
2013-04-03 11:15:41 -0400

    Test Push
    Added a get/set to the health component...
    I dont know if I have to push the .class file or the .java file so I
    hope this is the right one

--- src/com/enge/bullethell/Components/Health_Component.java
+++ src/com/enge/bullethell/Components/Health_Component.java
@@ -4,4 +4,13 @@ import com.artemis.Component;
 
 public class Health_Component extends Component {
 
+	private int health;
+	public int getHealth()
+	{
+		return health;
+	}
+	public void setHealth(int newHealth)
+	{
+		health = newHealth;
+	}
 }
