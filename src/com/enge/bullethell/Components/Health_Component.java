b6f0beb9e422aacf18cf6632d9e2e83a6e3643c1
Stephen Fenton <sfentonx@vt.edu>
2013-04-03 16:28:25 -0400

    pushing again just to check

--- src/com/enge/bullethell/Components/Health_Component.java
+++ src/com/enge/bullethell/Components/Health_Component.java
@@ -7,17 +7,17 @@ import com.artemis.Component;
  * @author SFenton
  * @version 2013.04.03
  */
-public class Health_Component extends Component {
+public class Health_Component extends Component 
+{
 
 	public int health;
-	
-<<<<<<< HEAD
+
 	/**
 	 * Empty constructor for the class.
 	 */
 	public Health_Component()
 	{
-		//Empty
+		this.health = 1;
 	}
 	
 	/**
@@ -26,13 +26,6 @@ public class Health_Component extends Component {
 	 */
 	public Health_Component(int health)
 	{
-=======
-	public Health_Component() {
-		this.health = 1;
-	}
-	
-	public Health_Component(int health) {
->>>>>>> 4790f0d6f01b66541186a13b698518ced23a5e74
 		this.health = health;
 	}
 }
