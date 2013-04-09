395c162f0ff46a317218d274cf44a4c4a1f5a9d9
Tyler D <Jensha@gmail.com>
2013-04-09 12:38:19 -0400

    Added mappers to movement system

--- Bullethell/src/com/enge/bullethell/Systems/Movement_System.java
+++ Bullethell/src/com/enge/bullethell/Systems/Movement_System.java
@@ -1,16 +1,25 @@
 package com.enge.bullethell.Systems;
 
 import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
 import com.artemis.Entity;
+import com.artemis.annotations.Mapper;
 import com.artemis.systems.EntityProcessingSystem;
+import com.enge.bullethell.Components.Position_Component;
+import com.enge.bullethell.Components.Velocity_Component;
 
 //components: position, velocity
 public class Movement_System extends EntityProcessingSystem{
+	
+	@Mapper ComponentMapper<Position_Component> positionM;
+	@Mapper ComponentMapper<Velocity_Component> velocityM;
+	
+	
 
     public Movement_System(Aspect aspect) {
         super(aspect);
     }
-
+    
     @Override
     protected void process(Entity entity) {
         //To change body of implemented methods use File | Settings | File Templates.
